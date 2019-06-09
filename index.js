41ecbe02e04de8d8bef9fada45d8d5ba5a9689ca
Patrick Kim <pkim050@gmail.com>
2019-06-09 12:53:56 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector("input").value;
+}
+
+function addNewElementAsLi() {
+  const temp = document.createElement("li");
+  temp.innerHTML = retrieveEmployeeInformation();
+  document.querySelector("ul.employee-list").appendChild(temp);
+}
+
+function addNewLiOnClick() {
+  const submit = document.querySelector('input[type="submit"]');
+  submit.addEventListener('click', addNewElementAsLi());
+  document.querySelector('input[name="name"]').value = "";
+}
+
+function clearEmployeeListOnLinkClick() {
+  const anchor = document.querySelector('a');
+  const ul = document.querySelector('ul');
+  anchor.addEventListener('click', () => {
+    ul.innerHTML = "";
+  });
+}
